9265d976d126ced4e220c95bb7617efee3089c81
abduqahhor7663 <167767860+abduqahhor7663@users.noreply.github.com>
2024-08-21 18:43:52 +0500

    update

--- shop2/shop/main/views.py
+++ shop2/shop/main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import *
 # Create your views here.
 
 class HomePageView(ListView):
     model = Product
     template_name = "index.html"
-
+    
+def products(request):
+    products = Product.objects.all()
+    cat = Category.object.all()
+    brands = Brand.object.all()
+    return render(request, {'cat':categories})
     
 def cartdetail(request):
     
